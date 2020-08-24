55390f3d1e6c0cf54f6824c88d2f0c7370db7d2d
ccf333 <7878006+ccf333@user.noreply.gitee.com>
2020-08-24 10:29:59 +0800

    dd

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "magetool",
+    version = "1.1.0",
+    keywords = ("pip", "pathtool","timetool", "magetool", "mage"),
+    description = "time and path tool",
+    long_description = "time and path tool",
+    license = "MIT Licence",
+
+    url = "https://gitee.com/ccf333/HTMLrun.git",
+    author = "ccf333",
+    author_email = "654721574@qq.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = []
+)
